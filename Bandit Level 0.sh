1417d9a9f38fb9a1fe33c7fc6043461e18b51a6a
Justin J. <justinthepatchnotes@gmail.com>
2025-02-09 09:26:46 -0500

    init commit

--- Bandit Level 0.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Hello, world!"
-
-ls -la
-cat -- -filename
-cat "file name with spaces"
-
-chmod 600 sshkey.private
-ssh -i sshkey.private banditX@bandit.labs.overthewire.org -p 2220
